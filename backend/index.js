9495134dbab9f4d70d417f691607e531243777aa
sigwaldostepan <sigwaldos@gmail.com>
2026-02-04 22:06:32 +0700

    reformat a little bit

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const dotenv = require("dotenv").config();
-const authRoutes = require("./routes/auth.js");
-const postsRoutes = require("./routes/posts.js");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const authRoutes = require('./routes/auth.js');
+const postsRoutes = require('./routes/posts.js');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = process.env.PORT;
 
@@ -12,11 +12,11 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use("/api/auth", authRoutes);
-app.use("/api/posts", postsRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postsRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Connected");
+app.get('/', (req, res) => {
+  res.send('Connected');
 });
 
 app.listen(port, () => {
--- frontend/src/lib/axiosInstance.js
+++ frontend/src/lib/axiosInstance.js
@@ -1,5 +1,7 @@
-import axios from "axios";
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:7000/api",
+  baseURL: `${API_URL}/api`,
 });
